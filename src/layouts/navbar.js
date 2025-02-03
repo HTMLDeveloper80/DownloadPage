4f04aec252f9c357777f6b0f53ac5aee58cd9307
HtmlDeveloper80 <miloszgoluch410@gmail.com>
2025-02-03 22:05:01 +0100

    First commit

---
+++ src/layouts/navbar.js
@@ -0,0 +1,13 @@
+import './navbar.css';
+
+function Navbar(){
+    return(
+            <div className='navbar'>
+                <button className='logo'>Logo</button>
+                <button className='sign-up'>Sign up</button>
+                <button className='log-in'>Log in</button>
+            </div>
+    )
+}
+
+export default Navbar;
