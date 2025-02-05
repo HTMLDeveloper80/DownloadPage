f8a0b4b68cbba7f2a62405c39c793f78b5dd124f
HtmlDeveloper80 <miloszgoluch410@gmail.com>
2025-02-05 17:14:34 +0100

    Footer UI added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './layouts/navbar';
 import MainScreen from './components/mainscreen';
+import Footer from './layouts/footer';
 
 function App() {
   return (
     <>
       <Navbar/>
       <MainScreen/>
+      <Footer/>
     </>
   );
 }
---
+++ src/layouts/footer.js
@@ -0,0 +1,16 @@
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer className='footer-contener'>
+            <div className="footer-buttons">
+                <a href="#" className="footer-button">About us</a>
+                <a href="#" className="footer-button">Terms</a>
+                <a href="#" className="footer-button">Support</a>
+            </div>
+            <p>Copyright</p>
+        </footer>
+    );
+}
+
+export default Footer;
